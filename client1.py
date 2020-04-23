b21da0c2e31e87486e778d68293094e4998481fd
karouia <alaedinekarouia@gmail.com>
2020-04-23 19:00:52 +0200

    maj client

--- client1.py
+++ client1.py
@@ -1,15 +1,82 @@
 # coding: utf-8
 
 import socket
+from string import *
+voyelle = ['a','e','i','o','u','y']
+consonne = ['b','c','d','f','g','h''j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+class Joueur:
+
+    def __init__(self,nom):
+        self.nom = nom
+        self.solde = 0
+        self.buzz = False
+
+    def buzzer(self):   ##UTILISATION DUN SELECT 1 THREAD POUR TOUS PUIS ENSUITE UN THREAD CHACUN
+        self.buzz = True
+
+    def proposervoyelle(self): ##1000 le prix du voyelle
+        if(self.solde>1000):
+            lettre = input("choisir votre lettre >>")
+            lettre = lettre.lower()
+            if lettre not in voyelle:
+                print("Eh oh! pas à nous ;-)")
+                self.proposervoyelle()
+
+            return lettre
+        return "desolé pas assez d'argent pour acheter une voyelle"
+
+    def proposerconsonne(self):
+        lettre = input("choisir votre consonne >>")
+        lettre = lettre.lower()
+        if lettre not in consonne:
+           print("Eh oh! pas à nous ;-)")
+           self.proposerconsonne()
+
+        return lettre
+
+    def proposerPhrase(self):
+        phrase = input("Que proposez vous  comme reponse? :")
+        return phrase
+
+
+
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("", 1111))
+s.connect(("localhost", 1111))   ## pour plus tard on proposera de choisir ou ce co , la pour l'instant on reste en local
+name = input("quel est votre nom:")
+j1 = Joueur(name)
 
-print("Le nom du fichier que vous voulez récupérer:")
-file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
-s.send(file_name.encode())
-file_name = 'data/%s' % (file_name,)
-r = s.recv(9999999)
-with open(file_name,'wb') as _file:
-    _file.write(r)
-print("Le fichier a été correctement copié dans : %s." % file_name)
+
+r = s.recv(99999) ## on recoit du serveur l'evenement
+
+
+if(r == 'choix'):  ## si cest l'evenement choix alors....
+
+    res = input("souhaitez vous acheter une voyelle :(oui/non")
+    if (res== 'oui'):
+        s.send(j1.proposervoyelle().encode('ascii'))
+    print("quelle consonne choisissez vous?")
+    s.send((j1.proposerLettre()).encode('ascii'))
+    final = input("souhaitez vous proposez une reponse ? oui/non")
+    if(final == 'yes'):
+        s.send((j1.proposerPhrase()).encode('ascii'))
+
+
+if(r == 'buzz'):
+    j1.buzz()
+    attente = s.recv(1024)
+    if (attente == "ok"):
+        j1.proposerPhrase()
+
+
+if(r == "defaite"):
+    print("vous avez perdu dommage")
+    s.close()
+
+
+if (r == "recap"):
+    recap = s.recv(2048)
+    print(recap)
+
--- server.py
+++ server.py
@@ -3,6 +3,58 @@
 import socket
 import threading
 
+from random import randint
+from time import sleep
+from string import ascii_letters
+import os
+from client1 import *
+
+class Jeu():
+    def __init__(self):
+        self.listeJoueur = []
+        self.roue = ['100', '200', '2000', 'Banqueroute']
+
+    def afficherListe(self):
+        for j in self.listeJoueur:
+            print(j.nom)
+    def ajouterJoueur(self,nom):
+        self.listeJoueur.append(nom)
+    def tournerLaRoue(self):
+        """Tourne la roue à votre place, vous faites pas mal !"""
+        lacase = randint(0,len(self.roue)-1)
+        return self.roue[lacase]
+
+    def choisirUneExpression(self):
+        """Choisis une expression dans la liste"""
+        f = open('enigmes/expressions.txt',mode='r',encoding='utf-8')
+        laPhrase  = f.readlines()
+        return laPhrase[randint(0,len(laPhrase)-1)]
+
+    def rapidite(self,str):
+        pass
+        """Le premier qui trouve gagne !\nC'est simple !"""
+        print("Christophe : Je vous propose une épreuve de rapidité, quel est le thème Victoria ?")
+        source = str.split(',')
+        theme,expression = source[1][:-1],source[0][:-1]
+        sleep(1)
+        print("Victoria : {} !".format(theme))
+        expressionCachee = self.cacherString(expression)
+        sleep(1)
+        print(expressionCachee)
+        print("Christophe : Top !")
+
+
+    def cacherString(self,str):
+        newstr = ''
+        for c in str:
+            if c in ascii_letters:
+                newstr += '_'
+            else:
+                newstr += c
+        return newstr
+
+
+game =Jeu()
 
 class ClientThread(threading.Thread):
 
@@ -17,20 +69,36 @@ class ClientThread(threading.Thread):
         print("Connexion de %s %s" % (self.ip, self.port,))
 
         r = self.clientsocket.recv(2048)
-        print("Ouverture du fichier: ", r, "...")
-        fp = open(r, 'rb')
-        self.clientsocket.send(fp.read())
 
-        print("Client déconnecté...")
+       ## self.clientsocket.send()
+
+
 
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcpsock.bind(("", 1111))
+clients = []
+
+def i_manage_clients():    #Function to manage clients
+    for client in clients:
+        client.send('Message to pass')
 
 while True:
-    tcpsock.listen(10)
-    print("En écoute...")
-    (clientsocket, (ip, port)) = tcpsock.accept()
-    newthread = ClientThread(ip, port, clientsocket)
-    newthread.start()
+    try:
+        tcpsock.listen(3)
+        print("En écoute...")
+        (clientsocket, (ip, port)) = tcpsock.accept()
+        clients.append(clientsocket) ##on remplit la liste des clients
+        newthread = ClientThread(ip, port, clientsocket)
+        newthread.start()
+    except KeyboardInterrupt:
+        socket.close()
+
+
+
+
+    ###etape1 buzz avec un select
+    ## un thread par joueur
+
+
