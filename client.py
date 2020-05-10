323450ccaf0a2f0644bcbefa32cca4b141a48386
karouia <alaedinekarouia@gmail.com>
2020-05-11 00:06:34 +0200

    azr

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import socket
 
+voyelle = ['a', 'e', 'i', 'o', 'u', 'y']
+consonne = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
 
-voyelle = ['a','e','i','o','u','y']
-consonne = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+
 class Joueur:
 
     def __init__(self):
@@ -10,14 +11,14 @@ class Joueur:
         self.solde = 0
         self.buzz = False
 
-    def buzzer(self):   ##UTILISATION DUN SELECT.select 1 THREAD POUR TOUS PUIS ENSUITE UN THREAD CHACUN
+    def buzzer(self):  ##UTILISATION DUN SELECT.select 1 THREAD POUR TOUS PUIS ENSUITE UN THREAD CHACUN
         self.buzz = True
 
     def canbuyVoyelle(self):
         return (self.solde > 1000)
 
-    def proposervoyelle(self): ##1000 le prix du voyelle
-        if(self.canbuyVoyelle()):
+    def proposervoyelle(self):  ##1000 le prix du voyelle
+        if (self.canbuyVoyelle()):
             lettre = input("Choisir votre lettre >>")
             lettre = lettre.lower()
             if lettre not in voyelle:
@@ -26,13 +27,12 @@ class Joueur:
             else:
                 return lettre
 
-
     def proposerconsonne(self):
         lettre = input("Choisir votre consonne >>  ")
         lettre = lettre.lower()
         if lettre not in consonne:
-           print("Eh oh! pas à nous ;-)")
-           self.proposerconsonne()
+            print("Eh oh! pas à nous ;-)")
+            self.proposerconsonne()
 
         return lettre
 
@@ -41,53 +41,45 @@ class Joueur:
         return phrase
 
 
-
-
-
 def debut():
     bienvenu = s.recv(1024)
     bienvenu = bienvenu.decode('utf-8')
-    print(bienvenu + "\n ") #Salut a toi l'ami (Référence à MisterRobot)
+    print(bienvenu + "\n ")  # Salut a toi l'ami (Référence à MisterRobot)
 
     them = s.recv(1024)
     them = them.decode('utf-8')
-    print(them) #X MANCHE + THEME + PHRAS
-
+    print(them)  # X MANCHE + THEME + PHRAS
 
     phrase = s.recv(1024)
     phrase = phrase.decode('utf-8')
-    print(phrase + "\n") #Theme 
-
-
+    print(phrase + "\n")  # Theme
 
     phrase2 = s.recv(1024)
     phrase2 = phrase2.decode('utf-8')
-    print(phrase2 + "\n") #Phrase caché sans indice
-
-    
+    print(phrase2 + "\n")  # Phrase caché sans indice
 
     phrase2 = s.recv(1024)
     phrase2 = phrase2.decode('utf-8')
-    print(phrase2 +": ") #Nous vous donner une lettre
+    print(phrase2 + ": ")  # Nous vous donner une lettre
 
-    #phrase4 = s.recv(1024)
-    #roueTourne = phrase4.decode('utf-8')
-    #print("\033[95m" + str(roueTourne) +"\033[0m" ) 
+    # phrase4 = s.recv(1024)
+    # roueTourne = phrase4.decode('utf-8')
+    # print("\033[95m" + str(roueTourne) +"\033[0m" )
     roue = s.recv(1024).decode('utf-8')
-    print("la roue tombe sur "+roue)
+    print("la roue tombe sur " + roue)
     gain = s.recv(1024)
     gain = gain.decode('utf-8')
-    print("\n Vous obtenez " + gain + " ! \n") #Gain de la roue
-    if(gain!="banqueroute"):
-        reponseClient('choix',gain)
+    print("\n Vous obtenez " + gain + " ! \n")  # Gain de la roue
+    if (gain != "banqueroute"):
+        reponseClient('choix', gain)
     else:
-        j1.solde=0
+        j1.solde = 0
         print("\nvous perdez votre solde\n")
 
 
-def reponseClient(r,gain):
-    temp=j1.solde+int(gain)
-    print("Vous avez : "+str(temp))
+def reponseClient(r, gain):
+    temp = j1.solde + int(gain)
+    print("Vous avez : " + str(temp))
     if (r == 'choix'):  ## si cest l'evenement choix alors....
         res = input("> Souhaitez vous acheter une voyelle :(oui/non) \n")
         if (res == 'oui'):
@@ -98,29 +90,28 @@ def reponseClient(r,gain):
 
         print("> Quelle consonne choisissez vous?")
         lettre = j1.proposerconsonne()
-        print("Vous avez choisi la lettre : "+lettre)
+        print("Vous avez choisi la lettre : " + lettre)
 
         s.send(bytes(lettre, "utf-8"))
 
         nblett = s.recv(1024).decode("utf-8")
-        print("\nvous avez trouvé "+ str(nblett) + " lettres\n")
+        print("\nvous avez trouvé " + str(nblett) + "lettres\n")
 
-        # j1.solde = j1.solde + (int(gain) * int(nblett))
-        # print("vous avez desormais " + str(j1.solde) +"€\n")
+        j1.solde = j1.solde + (int(gain) * int(nblett))
+        print("vous avez desormais " + str(j1.solde) + "€\n")
 
-        res3 = s.recv(1024).decode("utf-8") #Affichage de phraseCachee
+        res3 = s.recv(1024).decode("utf-8")  # Affichage de phraseCachee
         print(res3[0])
-        
+
         ##verifier si la lettre est bonne , plus recevoir la phrase avec la lettre
-        if(int(nblett) > 0):    
+        if (int(nblett) > 0):
             final = input("> Souhaitez vous proposez une reponse ? oui/non \n")
             if (final == 'oui'):
-                s.send(bytes("oui","utf-8"))
+                s.send(bytes("oui", "utf-8"))
                 s.send(bytes(j1.proposerPhrase(), "utf-8"))
                 print(s.recvfrom(1024)[0])
             else:
-                s.send(bytes("non","utf-8"))
-
+                s.send(bytes("non", "utf-8"))
 
 
 #####################################################
@@ -132,26 +123,22 @@ j1 = Joueur()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 name = 'localhost'
-s.connect((socket.gethostbyname(name), 9999))   # pour plus tard on proposera de choisir ou ce co , la pour l'instant on reste en local
-
-
-
-
+s.connect((socket.gethostbyname(name),
+           9999))  # pour plus tard on proposera de choisir ou ce co , la pour l'instant on reste en local
 
 debut()
-
 
 while True:
     running = True
     while running:
 
-        msg=s.recv(1024).decode('utf-8')
-        print(">"+msg)
-        if(str(msg)=="choix"):
-            value=s.recv(1024).decode('utf-8')
-            if(str(value)!="banqueroute"):
-                reponseClient("choix",value)
+        msg = s.recv(1024).decode('utf-8')
+        print(">" + msg)
+        if (str(msg) == "choix"):
+            value = s.recv(1024).decode('utf-8')
+            if (str(value) != "banqueroute"):
+                reponseClient("choix", value)
             else:
                 print("Banqueroute !")
-        elif(str(msg)=="fin"):
-            running=False
+        elif (str(msg) == "fin"):
+            running = False
